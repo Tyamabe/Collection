2edee9b79c59e260e0fd8ba4cce2bcf4ab8ea5b9
TaiheiYamabe <91378148+Tyamabe@users.noreply.github.com>
2024-03-27 09:17:50 +0900

    課題再提出

--- src/practice/Chapter5.java
+++ src/practice/Chapter5.java
@@ -10,10 +10,10 @@ public class Chapter5 {
 
 	public static void main(String[] args) throws Exception {
 		
-		//yyyy/MM/ddの形で表示したい
-		DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern("yyyy/MM/dd");
+		//yyyy-MM-ddの形で表示したい
+		DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
 		
-		List<Task> taskList = new ArrayList<Task>();//1つの要素にLocalDateとActionの二つをもつ空のリストを作成するには<>がStringとかではダメ。コンストラクタTask使用。ラムダ式
+		List<Task> taskList = new ArrayList<>();//1つの要素にLocalDateとActionの二つをもつ空のリストを作成するには<>がStringとかではダメ。コンストラクタTask使用。ラムダ式
 		
 		
 		//Actionを生成してtasklistに追加する
@@ -24,11 +24,11 @@ public class Chapter5 {
 		taskList.add(new Task(LocalDate.of(2021, 11, 9), "スクールの課題を解く。"));
 		
 		//Comparatorを使ってListをソートする方法→https://qiita.com/sinoofgan/items/89c200755131f13a3dbc
-		taskList.sort(Comparator.comparing(Task::getD, Comparator.naturalOrder()));
+		taskList.sort(Comparator.comparing(Task::getDate, Comparator.naturalOrder()));
 		
 		//拡張for文で順番に要素を取り出して処理
 		for(Task task : taskList) {
-			System.out.println(task.getD().format(dateTimeFormatter) + " " + task.getAction());
+			System.out.println(task.getDate().format(dateTimeFormatter) + "：" + task.getAction());
 			
 		}
 	}
--- src/practice/Task.java
+++ src/practice/Task.java
@@ -1,27 +1,24 @@
 package practice;
 
 import java.time.LocalDate;
-import java.time.format.DateTimeFormatter;
 
 
 public class Task {
-	//yyyy/MM/ddの形で表示したい
-	DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern("yyyy/MM/dd");
 	//日付を表すdと行動を表すactionの変数を用意する
-	private LocalDate d;
+	private LocalDate date;
 	private String action;
 	//コンストラクタ
-	public Task(LocalDate d, String action) {
-		this.d = d;
+	public Task(LocalDate date, String action) {
+		this.date = date;
 		this.action = action;
 	}
 	//getter, setter作っておく
-	public LocalDate getD() {
-		return d;
+	public LocalDate getDate() {
+		return date;
 	}
 	
-	public void setD(LocalDate d) {
-		this.d = d;
+	public void setDate(LocalDate date) {
+		this.date = date;
 	}
 	
 	public String getAction() {
